d83111617977b1811c37a6f19b2cc879f959b7a8
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2016-12-20 11:23:38 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 mkdir ~/Library/Developer/Xcode/Templates
 mkdir ~/Library/Developer/Xcode/Templates/Viper
-git clone git@github.com:ronanrodrigo/viper-templates.git
-cd viper-templates
-mv "Complete Interactor.xctemplate" ~/Library/Developer/Xcode/Templates/Viper
-cd ..
-rm -rf viper-templates
+git clone git@github.com:ronanrodrigo/viper-templates.git ~/Library/Developer/Xcode/Templates/Viper

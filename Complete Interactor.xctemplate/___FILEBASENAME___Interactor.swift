a1c81d69ddcb822f75a76cd56986c7c4d545c276
Ronan Rodrigo Nunes <ronan.nunes@concrete.com.br>
2016-12-20 11:17:32 -0200

    🎉

---
+++ Complete Interactor.xctemplate/___FILEBASENAME___Interactor.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct ___FILEBASENAMEASIDENTIFIER___Interactor {
+    private let presenter: ___FILEBASENAMEASIDENTIFIER___Presentable
+    private let gateway: <# Gateway Protocol #>
+}
---
+++ Complete Interactor.xctemplate/___FILEBASENAME___Presentable.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+protocol ___FILEBASENAMEASIDENTIFIER___Presentable {
+}
